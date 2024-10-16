eddcd0e8bd81d477e5b87eb0b25d55cad341ed59
mnabilaqsaaqsa <aqsamnabil744@gmail.com>
2024-10-16 13:41:19 +0800

    02-CONSTRUCTOR
    02-CONSTRUCTOR

--- App.java
+++ App.java
@@ -1,31 +1,25 @@
-class Mahasiswa{
+class Mahasiswa {
     String Nama;
     String NIM;
     String Prodi;
-    int Umur;
+    Integer umur;
     Double IPK;
-
 }
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        
-        //Membuat objek
-        Mahasiswa mhs_1 = new Mahasiswa();
 
-        //Memberikan nilai pada objek
-        mhs_1.Nama = "M. NABIL";
+        Mahasiswa mhs_1 = new Mahasiswa();
+        mhs_1.Nama = "M NABIL";
         mhs_1.NIM = "23241090";
         mhs_1.Prodi = "Pendidikan Teknologi Informasi";
-        mhs_1.Umur = 18;
+        mhs_1.umur = 18;
         mhs_1.IPK = 2.8;
-        
-        //Mencetak objek
-        System.out.println("Nama    : " + mhs_1.Nama);
-        System.out.println("NIM     : " + mhs_1.NIM);
-        System.out.println("Prodi   : " + mhs_1.Prodi);
-        System.out.println("Umur    : " + mhs_1.Umur);
-        System.out.println("IPK     : " + mhs_1.IPK);
+
+        System.out.println("Nama : " + mhs_1.Nama);
+        System.out.println("NIM : " + mhs_1.NIM);
+        System.out.println("Prodi : " + mhs_1.Prodi);
+        System.out.println("Umur : " + mhs_1.umur);
+        System.out.println("IPK : " + mhs_1.IPK);
     }
 }
